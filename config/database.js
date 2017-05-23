68c939dd5534566fc6c8f44692de1e00b1e058b5
Gerlis Alvarez Cardona <Galvarez@intergrupo.com>
2017-05-23 16:57:50 -0500

    change connection db password

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://gerlis18:geR3008511771.@ds131511.mlab.com:31511/platformhkr',
+    database: 'mongodb://gerlis18:geR3008511771@ds131511.mlab.com:31511/platformhkr',
     secret: 'issecret'
 }
