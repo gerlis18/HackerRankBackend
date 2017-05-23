3017fd5e06784ce29c6b37c884a7d994187b0caf
Gerlis Alvarez Cardona <Galvarez@intergrupo.com>
2017-05-23 16:51:21 -0500

    change connection db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/platform',
+    database: 'mongodb://gerlis18:geR3008511771.@ds131511.mlab.com:31511/platformhkr',
     secret: 'issecret'
 }
