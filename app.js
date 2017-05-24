656f54f358d0fac0491b83792f5633023c008bd1
root <root@localhost.localdomain>
2017-05-23 21:46:45 -0500

    enable cors origin

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ app.use(session({
     saveUninitialized: false
 }));
 
+//cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 require('./config/passport')(passport);
 
