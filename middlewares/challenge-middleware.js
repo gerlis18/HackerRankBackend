63b6ecaef3c21df82ffc142dcbfcaf01d42e5c44
Edwin Ricardo Romero Cabezas <Eromero@intergrupo.com>
2017-06-29 13:52:12 -0500

    Configuración inicial de pruebas unitarias

--- middlewares/challenge-middleware.js
+++ middlewares/challenge-middleware.js
@@ -9,12 +9,12 @@ module.exports.getTests = (callback) => {
 }
 
 module.exports.getTestById = (id, callback) => {
-    challengeModel.findById(id,callback);
+    challengeModel.findById(id, callback);
 }
 
 module.exports.getTestByTitle = (title, callback) => {
     const query = { title: title }
-    challengeModel.findOne(query,callback);
+    challengeModel.findOne(query, callback);
 }
 
 module.exports.updateTest = (id, test, callback) => {
---
+++ test/middlewares/challenge-detail.middlewareTests.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+var challenge_detail = require("../../middlewares/challenges-detail-middleware");
+
+describe("challenge-detail-middleware", function() {
+    describe("#getUserChallengeScore", function() {
+        it("registrosDentroTiempoPrueba_obtieneScoreDentroDeMayor", function() {
+
+        });
+        it("registrosFueraTiempoPrueba_obtieneScoreMenorAMaximoDentroTiempo", function() {
+
+        });
+    });
+});
